6693701a59b8594b67b33b86b4244644935e75ec
ZXxNetoxXZ <49221536+ZXxNetoxXZ@users.noreply.github.com>
2019-11-25 13:01:50 -0600

    Update BankAccount.java

--- JAVA/BankAccount.java
+++ JAVA/BankAccount.java
@@ -6,10 +6,8 @@ public class BankAccount {
 	/*
 	Declaring the instance variables for the class
 	@param finalBalance holds the total money of the object bank account
-	@param interestRate is passed to the interest method and is always constant
 	*/
 	private double finalBalance; 
-	private final double interestRate = .075;
 	/*
 	creating an empty BankAccount object and setting its finalBalance to 0
 	@param finalBalance is set automatically to 0 when no other parameters are inputted.
@@ -45,17 +43,5 @@ public class BankAccount {
 	*/
 	public double getBalance() {
 		return finalBalance; // returns the actual value of the object
-	}
-	/*
-	method for calculating and updating the finalBalance for an invested amount of balance with interest
-	@param amount is to be applied the interest
-	@param term is the time period or frequency of the applied interest
-	@param intest performs the calculations for the interest
-	@param total gets the actual value to be added to the bank account
-	*/
-	public void interest(double term, double amount) {
-		double intest = (Math.pow((1+(interestRate/term)), term)) - 1;
-		double total = intest*amount;
-		this.deposit(total); //implicit variable
 	}
 }
