a38642a34e26661fb268be4c5f35f0e966e76591
ZXxNetoxXZ <continental.lelgg.2b@gmail.com>
2020-03-11 08:13:40 -0600

    e

--- Projects/CSIA-Coding/TillNow/AccesingMailAccounts.py
+++ Projects/CSIA-Coding/TillNow/AccesingMailAccounts.py
@@ -1,25 +1,39 @@
-m_list = ["a01654419@itesm.mx", "a01654106@itesm.mx", "a01654202@itesm.mx"]  #  mails of all the members in the school
-n_dict = {
-    "a01654419@itesm.mx": 'Password1',
-    "a01654202@itesm.mx": 'Password3',
-    "a01654106@itesm.mx": 'Password1'
-}
-# dictionary of all the already existing accounts with usernames and passwords
+"""
 
-id_mail = {"a01654106@itesm.mx": 'Password1'}
+Author: ZXxNetoxXZ
+Tentative program to login or register new members of the campus into the web page.
+
+"""
+
+m_list = ["a01654419@itesm.mx", "a01654106@itesm.mx", "a01654202@itesm.mx"]  #  list of all mails in the school by my client
+mp_dict = { # usernames(mails) and passwords for registered accounts
+    "a01654419@itesm.mx": "Password1",
+    "a01654202@itesm.mx": "Password3",
+    }
+
+def login_toPage():
+    emp_dict = dict() #entered mail and password dict
+    # Both username and password should be associated with a GUI element
+    username = input()
+    password = input()
+    emp_dict[username] = password
+    if username in mp_dict.keys() and password == mp_dict.get(username):
+        print("Login in...")
+        # Access the next page
+    else:
+        print("The username or password is wrong! Try again!")
 
 
-def create_user(m_list, n_dict, id_mail):
-    for key in n_dict:
-        if list(id_mail.keys())[0] == key:
-            print(list(id_mail.keys())[0])
-            return("This mail is already registered!")
-            break
-    for i in range(len(m_list)):
-        if  m_list[i] == list(id_mail.keys())[0]:
-            print(m_list[i])
-            n_dict.update(id_mail)
-            return n_dict
-            print("It has now updated!")
-
-print(create_user(m_list, n_dict, id_mail))
+def register():
+    # Again, associate both username to GUI elements
+    emp_dict = dict()
+    username = input()
+    password = input()
+    emp_dict[username] = password
+    print(emp_dict)
+    if username in mp_dict.keys() and username in m_list:
+        print("The mail is already registered") # Checking for username in m_list is a layer of security
+    elif username not in m_list:
+        print("This mail does not exist! Be careful")
+    else:
+        mp_dict.update(emp_dict)
