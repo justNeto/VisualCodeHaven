b0768fda2dc4f561bd121ea2b156dca7e8be3732
justNeto <49221536+justNeto@users.noreply.github.com>
2021-03-10 14:24:47 -0600

    Update and rename RelationsCheatCode to RelationsCheatCode.py

--- Python/CheatScripts/RelationsCheatCode.py
+++ Python/CheatScripts/RelationsCheatCode.py
@@ -1,12 +1,12 @@
 import pandas as pd
 df = pd.read_excel(r"excel_file.xslx")
 df = df.truncate(before=120, after=149)
-red = df.loc[df['Uso'] == "Florero"]['#'].tolist()
+aux1 = df.loc[df['Uso'] == "Florero"]['#'].tolist()
 aux = list()
 # [1, 2, 4, 54, 65, 8]
-for i in range(len(red)):
-    for j in range(i+1, len(red)):
-        aux.append([red[i], red[j]])
+for i in range(len(aux1)):
+    for j in range(i+1, len(aux1)):
+        aux.append([aux1[i], aux1[j]])
 aux = ' '.join(map(str, aux))
 aux = aux.replace("[", "(")
 print(aux.replace("]", ")"))
